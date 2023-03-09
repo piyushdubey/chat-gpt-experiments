77e85e6013bfef829ef3b615b869a1c693dff3c4
piyushdubey <piyushdubey07@gmail.com>
2023-03-09 15:57:12 -0500

    Simple static text chat gpt prompt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+import openai
+
+openai.api_key = "sk-"
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!";
+    return "Hello, World!"
+
+@app.route('/penguin')
+def penguin():
+    turbo = "gpt-3.5-turbo"
+    davinci_text = "text-davinci-003"
+    completion = openai.ChatCompletion.create(model=turbo, messages=[{"role": "user", "content":"Summarize the theory of relativity for a 5 year old"}])
+    return completion.choices[0].message.content
